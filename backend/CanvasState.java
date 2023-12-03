2d5e00d115b727ebee62e9bfafa1728eb5e61250
Olivia Rodrigue <orodrigue@itba.edu.ar>
2023-12-03 19:58:56 -0300

    Comienzo a modelar selectedFigures

--- backend/CanvasState.java
+++ backend/CanvasState.java
@@ -6,19 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CanvasState {
+    //ordenadas por cual aparece al frente:
+    //con la primera figura siendo la del fondo y la ultima la que se encuentra más al frente
 
-    private final List<Figure> list = new ArrayList<>();
+    private final List<Figure> figures = new ArrayList<>();
+    private List<Figure> selectedFigures= new ArrayList<>();
 
+    //agrego figura
     public void addFigure(Figure figure) {
-        list.add(figure);
+        figures.add(figure);
     }
 
+    //elimino figura
     public void deleteFigure(Figure figure) {
-        list.remove(figure);
+        figures.remove(figure);
+    }
+
+    //devuelvo copia de las figuras que estan seleccionadas
+    public List<Figure> SelectedFigures(){ return new ArrayList<>(selectedFigures);}
+    public boolean SelectedFiguresIsEmpty(){
+        return selectedFigures.isEmpty();
     }
 
     public Iterable<Figure> figures() {
-        return new ArrayList<>(list);
+        return new ArrayList<>(figures);
+    }
+
+    public void moveSelectedFigures(double diffX, double diffY) {
+        for(Figure selectedFigure : selectedFigures)
+            selectedFigure.move(diffX, diffY);
     }
 
 }
