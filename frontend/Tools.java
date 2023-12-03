40f10209b125d9e131c2bdb8b1d8354a83602283
Pilar <pfrutos@itba.edu.ar>
2023-12-03 20:45:53 -0300

    "cambio a constantes"

--- frontend/Tools.java
+++ frontend/Tools.java
@@ -12,33 +12,44 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 public class Tools extends VBox {
+    private final static String SELECCIONAR = "Seleccionar";
+    private final static String RECTANGULO = "Rectángulo";
+    private final static String CIRCULO = "Círculo";
+    private final static String CUADRADO = "Cuadrado";
+    private final static String ELIPSE = "Elipse";
+    private final static String BORRAR = "Borrar";
+    private final static int MIN_WIDTH = 90;
+    private final static int INSETS_PADDING = 5;
+    private final static int PREF_WIDTH = 100;
+    private final static int SPACING = 10;
 
     // Botones Barra Izquierda
-    private final ToggleButton selectionButton = new ToggleButton("Seleccionar");
-    private final FigureButton rectangleButton = new RectangleButton("Rectángulo");
-    private final FigureButton circleButton = new CircleButton("Círculo");
-    private final FigureButton squareButton = new SquareButton("Cuadrado");
-    private final FigureButton ellipseButton = new EllipseButton("Elipse");
-    private final ToggleButton deleteButton = new ToggleButton("Borrar");
+    private final ToggleButton selectionButton = new ToggleButton(SELECCIONAR);
+    private final FigureButton rectangleButton = new RectangleButton(RECTANGULO);
+    private final FigureButton circleButton = new CircleButton(CIRCULO);
+    private final FigureButton squareButton = new SquareButton(CUADRADO);
+    private final FigureButton ellipseButton = new EllipseButton(ELIPSE);
+    private final ToggleButton deleteButton = new ToggleButton(BORRAR);
 
     private final FigureButton[] figureButtonsArr = {rectangleButton, circleButton, squareButton, ellipseButton};
-    private final Color defaultFillColor = Color.YELLOW;
-    private final ColorPicker fillColorPicker = new ColorPicker(defaultFillColor);
+    private final static Color DEFAULT_FILL_COLOR = Color.YELLOW;
+    private final ColorPicker fillColorPicker = new ColorPicker(DEFAULT_FILL_COLOR);
+    private final static Cursor HAND = Cursor.HAND;
     public Tools(){
 
         ToggleButton[] toolsArr = {selectionButton, rectangleButton, circleButton, squareButton, ellipseButton, deleteButton};
         ToggleGroup tools = new ToggleGroup();
         for (ToggleButton tool : toolsArr) {
-            tool.setMinWidth(90);
+            tool.setMinWidth(MIN_WIDTH);
             tool.setToggleGroup(tools);
-            tool.setCursor(Cursor.HAND);
+            tool.setCursor(HAND);
         }
         getChildren().addAll(toolsArr);
         getChildren().add(fillColorPicker);
-        setPadding(new Insets(5));
+        setPadding(new Insets(INSETS_PADDING));
         setStyle("-fx-background-color: #999");
-        setPrefWidth(100);
-        setSpacing(10);
+        setPrefWidth(PREF_WIDTH);
+        setSpacing(SPACING);
     }
 
     public void figureButtonAction(EventHandler<ActionEvent> action) {
